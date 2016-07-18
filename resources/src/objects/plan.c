37f0160dc9a40da3a818b54dcbcc74e04a6d7dc5
Gordon RASS-KWASI <grass-kw@e3r2p8.42.fr>
2016-07-18 15:25:11 +0200

    scene rotation et shadow

--- resources/src/objects/plan.c
+++ resources/src/objects/plan.c
@@ -6,7 +6,7 @@
 /*   By: grass-kw <grass-kw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/19 15:49:59 by grass-kw          #+#    #+#             */
-/*   Updated: 2016/07/08 16:18:21 by grass-kw         ###   ########.fr       */
+/*   Updated: 2016/07/18 14:33:43 by grass-kw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ double	plan(t_obj obj, t_vector3d start, t_vector3d ray)
 {
 	double k;
 
-	k = -(-obj.origin.x * start.x + obj.origin.y * start.y + obj.origin.z
-		* start.z + obj.size) / (-obj.origin.x * ray.x + obj.origin.y
+	k = -(obj.origin.x * start.x + obj.origin.y * start.y + obj.origin.z
+		* start.z + obj.size) / (obj.origin.x * ray.x + obj.origin.y
 		* ray.y + obj.origin.z * ray.z);
 	if (k > ZERO)
 		return (k);
