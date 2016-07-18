254bcca31cd88ef0d28b372bc446939ad26b13b9
Gordon RASS-KWASI <grass-kw@e3r2p8.42.fr>
2016-07-18 17:09:43 +0200

    scene 4 5 6 fonctionnel

--- resources/src/objects/plan.c
+++ resources/src/objects/plan.c
@@ -6,7 +6,7 @@
 /*   By: grass-kw <grass-kw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/19 15:49:59 by grass-kw          #+#    #+#             */
-/*   Updated: 2016/07/18 14:33:43 by grass-kw         ###   ########.fr       */
+/*   Updated: 2016/07/18 16:11:36 by grass-kw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ double	plan(t_obj obj, t_vector3d start, t_vector3d ray)
 {
 	double k;
 
-	k = -(obj.origin.x * start.x + obj.origin.y * start.y + obj.origin.z
+	k = -1 * (obj.origin.x * start.x + obj.origin.y * start.y + obj.origin.z
 		* start.z + obj.size) / (obj.origin.x * ray.x + obj.origin.y
 		* ray.y + obj.origin.z * ray.z);
-	if (k > ZERO)
+	if (k > 0.001)
 		return (k);
 	return (ZERO);
 }
